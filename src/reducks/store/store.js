048a0a91fcf9e0e46a85e802531e4644b4da0ab9
applego <applegorillappa@gmail.com>
2021-01-26 00:42:13 +0900

    #7...URLに応じたコンポーネントを表示しよう - middlewareの導入 - StoreとRouterの接続 - Routerコンポーネントを作る - Routerコンポーネントを使う - templatesファイルの作成   Login.jsx, Home.jsx - npm install --save history@4.10.1 - Login.jsxのconsole.logで react-reduxのuseSelectorを使って(useSelctro(state=>state).routerを記録

--- src/reducks/store/store.js
+++ src/reducks/store/store.js
@@ -1,17 +1,22 @@
 import {
   createStore as reduxCreateStore,
-  combineReducers
+  combineReducers,
+  applyMiddleware
 } from 'redux';
-
+import {connectRouter, routerMiddleware} from 'connected-react-router';
 
 // Import reducers
 // import { ProductsReducer } from '../products//reducers';
 import { UsersReducer } from '../users/reducers';
 
-export default function createStore() {
+export default function createStore(history) {
   return reduxCreateStore( //reduxのcreateStoreメソッドの別名
     combineReducers({
+      router:connectRouter(history),
       users:UsersReducer
-    })
+    }),
+    applyMiddleware(
+      routerMiddleware(history)
+    )
   )
 }
