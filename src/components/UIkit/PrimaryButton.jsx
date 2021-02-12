431ba49bd16b82aa295f9cfed378e68b9be9f25e
applego <applegorillappa@gmail.com>
2021-02-12 10:00:35 +0900

    🐛：height綴り修正

--- src/components/UIkit/PrimaryButton.jsx
+++ src/components/UIkit/PrimaryButton.jsx
@@ -1,26 +1,30 @@
 import React from 'react';
 import Button from '@material-ui/core/Button';
-import { makeStyles } from "@material-ui/styles";
+import { makeStyles } from '@material-ui/styles';
 
 const useStyles = makeStyles({
-  "button": {
-    backgroundColor: "#4dd0e1",
-    color: "#000",
+  button: {
+    backgroundColor: '#4dd0e1',
+    color: '#000',
     fontSize: 16,
-    hheight: 48,
+    height: 48,
     marginBottom: 16,
-    width: 256
-  }
-})
+    width: 256,
+  },
+});
 
 const PrimaryButton = (props) => {
   const classes = useStyles();
 
   return (
-    <Button className={classes.button} variant="contained" onClick={()=>props.onClick()}>
+    <Button
+      className={classes.button}
+      variant='contained'
+      onClick={() => props.onClick()}
+    >
       {props.label}
     </Button>
-  )
-}
+  );
+};
 
 export default PrimaryButton;
